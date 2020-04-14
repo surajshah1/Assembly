1c4c1b95f4bada78ea90aa102f0fc44bfaf3513e
surajshah1 <surajshah535@gmail.com>
2020-04-13 23:38:59 -0700

    making progress on cw#3

--- classwork/3/sd.asm
+++ classwork/3/sd.asm
@@ -12,8 +12,9 @@ variance:      ;prologue
   cmp rsi, 0    ; check the second array
   je rexit      ; jump to exit
   cmp rdx, 0	; check the size of array
-  je rexit	; jump to exit
-
+  re rexit	; jump to exit
+  
+  mov r15, rdi  ; save rdi here -- breakpoint here and check r15  
   mov r8d, 0
   mov r9d, 0
   mov r10d, 0
@@ -36,14 +37,10 @@ sumLoop:
   cdq			; convert 
   mov ebx, 10 		; mov divisor into ebx
   idiv ebx		; result should be in eax ; ignore remainder 
-			; working till here we know that the average is in eax
-
-;reset position of rdi source array pointer
-  push rdx
-  imul rdx, 4		; multiply the size by 4 and then do rdi - this
-  sub rdi, rdx		; position is now reset
-  pop rdx
+			; working till here ---  average is in eax
+  
   mov rcx, rdx		; reset the length value which I decrement in rcx
+  mov rdi, r15		; -- breakpoint here
 
 indexMinusAverage:  
   mov r8d, dword [rdi]	; get the current element in the source array 
@@ -56,9 +53,9 @@ indexMinusAverage:
   jne indexMinusAverage ; jump back into this loop  
    
   mov eax, r10d		; prepare for division
-  ;cdq
-  ;mov ebx, 10
-  ;idiv ebx		; variance will be in eax 
+  cdq
+  mov ebx, 10
+  idiv ebx		; variance will be in eax 
 
 rexit:          ;epilogue
  pop rcx

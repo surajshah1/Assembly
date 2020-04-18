9c108ed9a0377d202ccda980293bf96a38d7b12c
surajshah1 <surajshah535@gmail.com>
2020-04-17 23:11:00 -0700

    added c function

--- classwork/classwork4/abs.c
+++ classwork/classwork4/abs.c
@@ -3,16 +3,20 @@
 
 //rax 		//rdi
 unsigned int absolute_value(int32_t k);
+//void absolute_value_in_C(int32_t &i);
+
+void absolute_value_in_C(int32_t *j)
+{
+	if (*j < 0)
+		*j = *j * -1;
+}
  
 int main(int argc, char **argv)
 {
-	
 	int32_t i = -1983;
 	int32_t j = i;
+	absolute_value_in_C(&j);
 	uint32_t k = absolute_value(i);
-	
-	if (j < 0)
-		j = j * -1;
 	
 	printf("The original value is: %d \n", i);
 	printf("The absolute value in C is: %d \n", j);

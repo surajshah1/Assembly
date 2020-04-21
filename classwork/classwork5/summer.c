2886d93ee2dcef4f931ea5e29c3ca597f8505d82
surajshah1 <surajshah535@gmail.com>
2020-04-20 21:26:05 -0700

    base cw#5

---
+++ classwork/classwork5/summer.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+float sum(float* arr, int size);
+
+
+int main(int argc, char** argv) {
+float arr[10] = {1.5, 4.2, 16.1, 45.2, 2.1, 3.4, 3.14159265, 1.619, 2.71827, 6.022};
+float x = sum(arr,10);
+printf("Sum is :%f\n", x);
+return 0;
+}

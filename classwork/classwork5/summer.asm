2b9e063da8c599d2f8bac69847ef53f165cfd7b1
surajshah1 <surajshah535@gmail.com>
2020-04-22 01:25:11 -0700

    completed cw#5

--- classwork/classwork5/summer.asm
+++ classwork/classwork5/summer.asm
@@ -1,2 +1,28 @@
+global summer
 
-global
+section .text
+
+;need to use XMM registers for floats
+summer:				; prologue
+
+	push rbp
+	mov rbp, rsp
+	
+	cmp rdi, 0		; verify input array
+	je rexit
+	cmp rsi, 0		; verify array size
+	je rexit 
+	
+	sub rsi, 1 		; to execute loop 10 times (starts from 0)
+
+sumLoop:
+	movss xmm1, dword[rdi]	; mov from source array 
+	addss xmm0, xmm1	; add to return register
+	add rdi, 4		; increment array
+	sub rsi, 1		; decrement size
+	cmp rsi, 0		; compare size
+	jne sumLoop
+
+rexit:				; epilogue
+	pop rbp
+	ret
